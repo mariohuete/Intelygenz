86dd2b5e8cee2cb15f2deb638760684f5ca0d4df
mario.huete <mario.huete@planetmedia.es>
2015-02-10 09:50:33 +0100

    [NEW] broadcastReceiver to check changes in connection

---
+++ app/src/main/java/com/mariohuete/rss_reader/utils/UpdateReceiver.java
@@ -0,0 +1,26 @@
+package com.mariohuete.rss_reader.utils;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * Created by mariobama on 10/02/15.
+ */
+public class UpdateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        boolean isConnected = activeNetInfo != null && activeNetInfo.isConnectedOrConnecting();
+        if (isConnected)
+            Log.i("NET", "connecte" + isConnected);
+        else Log.i("NET", "not connecte" +isConnected);
+    }
+}
